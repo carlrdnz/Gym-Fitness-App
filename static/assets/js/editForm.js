dcd53b678fb94a85c20957d71cca794810685e2c
John Carlo Villegas <carlrdnz@gmail.com>
2023-10-17 21:21:35 +0800

    [MERGE]Merged editForm.js and profileChange.js into one JS FILE

--- static/assets/js/editForm.js
+++ static/assets/js/editForm.js
@@ -83,6 +83,9 @@ document.getElementById('saveProfileBtn').addEventListener('click', function() {
     // Send the data
     xhr.send('new_fullname=' + newFullname + '&new_email=' + newEmail + '&new_phonenumber=' + newPhonenumber + '&new_address=' + newAddress);
 
+    var profilePictureInput = document.getElementById('profilePictureInput');
+    profilePictureInput.classList.add('d-none');
+
     // Update the spans with the new values
     document.getElementById('fullname').textContent = newFullname;
     document.getElementById('email').textContent = newEmail;
@@ -108,3 +111,40 @@ document.getElementById('saveProfileBtn').addEventListener('click', function() {
     document.getElementById('saveProfileBtn').classList.add('d-none');
     document.getElementById('cancelEditBtn').classList.add('d-none');
 });
+
+// Add an event listener to the "Edit" button
+document.getElementById('editProfileBtn').addEventListener('click', function() {
+    // Toggle the visibility of the file input
+    var profilePictureInput = document.getElementById('profilePictureInput');
+    profilePictureInput.classList.toggle('d-none');
+});
+
+// Add an event listener to the "Cancel" button
+document.getElementById('cancelEditBtn').addEventListener('click', function(event) {
+    // Prevent the "Cancel" button click event from reaching the document
+    event.stopPropagation();
+    event.preventDefault();
+
+    // Ensure the profile picture input remains hidden
+    var profilePictureInput = document.getElementById('profilePictureInput');
+    if (!profilePictureInput.classList.contains('d-none')) {
+        profilePictureInput.classList.add('d-none');
+    }
+});
+
+// Listen for changes in the file input
+document.getElementById('profilePictureInput').addEventListener('change', function() {
+    var profilePicture = document.getElementById('profilePicture');
+    var fileInput = this;
+
+    if (fileInput.files && fileInput.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            profilePicture.src = e.target.result; // Update the profile picture with the selected image
+        };
+
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+});
+
